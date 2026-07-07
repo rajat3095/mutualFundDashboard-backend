52ba7935bb598fa3f85d43ac25ca41ebcaf804a3
Rajat Gandhi <raj.jain3006@gmail.com>
2026-07-07 22:56:04 +0530

    #update
    - feature added to upload the json file and bulk create of investor in database

--- src/routes/investors.js
+++ src/routes/investors.js
@@ -5,11 +5,15 @@ import {
   portfolioSummary,
   getSchemeWiseSummary,
 } from "../controllers/investorController.js";
+import multer from "multer";
 
 const router = express.Router();
 
+// 1. Configure multer (memory storage is fine for this)
+const upload = multer({ storage: multer.memoryStorage() });
+
 router.get("/", getAllInvestors);
-router.post("/createInvestors", createInvestorsBulk);
+router.post("/createInvestors", upload.single("file"), createInvestorsBulk);
 router.get("/:investorId/summary", portfolioSummary);
 router.get("/:investorId/schemeSummary", getSchemeWiseSummary);
 
